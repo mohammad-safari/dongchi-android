36b419c41a224ceb994a7d32f68344980e304359
Mohammad-Safari <msafari8051@gmail.com>
2023-11-24 13:08:41 +0330

    [FIX]revert changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "ce.bhesab.dongchi"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "ce.bhesab.dongchi"
-        minSdk = 34
-        targetSdk = 34
+        minSdk = 28
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
@@ -41,7 +41,6 @@ android {
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
-        kotlinCompilerVersion = "1.5.21"
     }
     packaging {
         resources {
@@ -51,21 +50,19 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-compose:1.8.1")
-    implementation("androidx.compose.foundation:foundation:1.5.4")
-    implementation("androidx.compose.material:material:1.5.4")
-    implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-    implementation("androidx.core:core-ktx:1.12.0")
+
+    implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.activity:activity-compose:1.7.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.navigation:navigation-compose:2.6.0")
+    implementation("com.google.android.material:material:1.8.0")
+    implementation("co.yml:ycharts:2.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -73,5 +70,4 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation("androidx.navigation:navigation-compose:2.7.5")
 }
--- app/src/main/java/ce/bhesab/dongchi/MainActivity.kt
+++ app/src/main/java/ce/bhesab/dongchi/MainActivity.kt
@@ -3,26 +3,13 @@ package ce.bhesab.dongchi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.rememberNavController
+import ce.bhesab.dongchi.screen.DashboardScreen
 import ce.bhesab.dongchi.theme.DongchiTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,15 +17,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DongchiTheme {
+                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val navController = rememberNavController()
-                    NavHost(
-                        navController = navController,
-                        startDestination = "home"
-                    ) { TODO("not yet implemented") }
+                    Root()
                 }
             }
         }
@@ -46,79 +30,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun DebtScreen() {
-    Column(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        // Overall Debt Status
-        TotalDebt()
-
-        // List of Individual Debts
-        DebtList()
-
-        // Bottom Button
-        StartNewGroupButton()
-    }
+fun Root(modifier: Modifier = Modifier) {
+    val dashboardScreen = DashboardScreen()
+    dashboardScreen.Dashboard(navController = null, overview = dashboardScreen.mockOverview)
 }
 
+@Preview(showBackground = true, showSystemUi = true, locale = "fa")
 @Composable
-fun TotalDebt() {
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(100.dp) // Adjust as needed
-            .background(Color.Red)
-    ) {
-        Text(
-            text = "Overall, you owe $491.00",
-            //style = MaterialTheme.typography.h6,
-            color = Color.White
-        )
+fun GreetingPreview() {
+    DongchiTheme {
+        Root()
     }
-}
-
-@Composable
-fun DebtList() {
-    // You'll want to replace this with a dynamic list, probably using LazyColumn
-    Column {
-        DebtItem(name = "913", amount = 692.00, isOwed = true)
-        DebtItem(name = "Sharafi 1401 fall", amount = 201.00, isOwed = false)
-        // Add more items...
-    }
-}
-
-@Composable
-fun DebtItem(name: String, amount: Double, isOwed: Boolean) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-    ) {
-        Text(
-            text = if (isOwed) "You owe $name $$amount" else "$name owes you $$amount",
-            //style = MaterialTheme.typography.subtitle1
-        )
-        // Add icons or other elements as needed
-    }
-}
-
-@Composable
-fun StartNewGroupButton() {
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-    ) {
-        Button(onClick = { /* Handle click */ }) {
-            Text("Start a new group")
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DebtScreen()
 }
