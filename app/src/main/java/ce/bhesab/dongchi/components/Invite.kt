2e1461ea107a9b797311d01e0cb760c21e81e28e
sarvenaz-srv <sarvghad79@gmail.com>
2023-11-24 22:46:03 +0330

    add farsi strings

--- app/src/main/java/ce/bhesab/dongchi/components/Invite.kt
+++ app/src/main/java/ce/bhesab/dongchi/components/Invite.kt
@@ -13,42 +13,46 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import ce.bhesab.dongchi.R
 
 @Composable
 fun Invite(){
-    Column(modifier = Modifier.padding(15.dp).fillMaxWidth(),
+    Column(modifier = Modifier
+        .padding(15.dp)
+        .fillMaxWidth(),
         verticalArrangement = Arrangement.SpaceBetween) {
         Row(modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically){
             Box {
-                Text(text = "code")
+                Text(text = stringResource(id = R.string.invite_code))
             }
         }
         Spacer(modifier = Modifier.height(15.dp))
         Row(modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Center){
-            Text(text = "You can invite others using this link")
+            Text(text = stringResource(R.string.invite_description))
         }
         Spacer(modifier = Modifier.height(15.dp))
         Row(modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Center){
             Button(onClick = { /*TODO*/ }) {
-                Text(text = "Invite")
+                Text(text = stringResource(id = R.string.invite))
             }
         }
         Row(modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Center){
             Button(onClick = { /*TODO*/ }) {
-                Text(text = "Later")
+                Text(text = stringResource(id = R.string.later))
             }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, locale = "fa")
 @Composable
 fun InvitePreview(){
     Invite()
