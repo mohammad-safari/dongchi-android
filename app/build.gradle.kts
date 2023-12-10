bad08ad6323af69a411de80a09ed345763f78ba4
amirsallah <amir1381basketball@gmail.com>
2023-12-11 01:02:41 +0330

    create CreateGroupScreen.kt create JoinGroup.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "ce.bhesab.dongchi"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "ce.bhesab.dongchi"
@@ -55,7 +55,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
+    implementation("androidx.compose.material:material-icons-extended:1.5.4")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
---
+++ app/src/main/java/ce/bhesab/dongchi/screen/CreateGroupScreen.kt
@@ -0,0 +1,100 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package ce.bhesab.dongchi.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Groups
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ce.bhesab.dongchi.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CreateGroupScreen() {
+    var nameGroup by remember {
+        mutableStateOf("")
+    }
+    val sheetState = rememberModalBottomSheetState()
+    var isSheetOpen by rememberSaveable {
+        mutableStateOf(false)
+    }
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+
+        if (isSheetOpen) {
+            ModalBottomSheet(
+                sheetState = sheetState,
+                onDismissRequest = {
+                    isSheetOpen = false
+                }
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        OutlinedTextField(
+                            value = nameGroup,
+                            onValueChange = { nameGroup = it },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 8.dp),
+                            label = { Text(stringResource(R.string.email)) },
+                            leadingIcon = {
+                                Icon(Icons.Default.Groups, contentDescription = null)
+                            }
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(text = stringResource(id = R.string.join))
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+@Preview(showSystemUi = true, locale = "fa")
+@Composable
+fun Salah() {
+    CreateGroupScreen()
+}
